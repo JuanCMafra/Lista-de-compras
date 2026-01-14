f16300cb75285ebd405ae866a868a5f4243bea0d
Mafra <juan.carlos2113@hotmail.com>
2026-01-14 16:28:15 -0300

    creating add item and trash

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-let item = document.getElementById("item")
-let addBtn = document.getElementById("add-item")
+const item = document.getElementById("item")
+const list = document.querySelector("ul")
+const btn = document.getElementById("add-item")
 
-addBtn.addEventListener("click", (event) => {
-  event.preventDefault()
+btn.addEventListener("click", addItem)
 
-})
+let counter = 0
+
+item.addEventListener("keydown", function (e) {
+  if (e.key === "enter") {
+    addItem();
+  }
+});
+
+function addItem() {
+
+const item = document.getElementById("item").value.trim()
+
+if (item === "") {
+  alert("Por favor, insira um item válido.")
+  return
+}
+
+counter++
+
+const newItem = document.createElement("li")
+const itemName = document.createElement("label")
+const checkbox = document.createElement("input")
+const deleteButton = document.createElement("button")
+const deleteIcon = document.createElement("img")
+
+deleteButton.onclick = function() {
+  list.removeChild(newItem)
+}
+
+const itemId = `item${counter}`
+deleteIcon.src = "assets/trash.svg"
+checkbox.type = "checkbox"
+checkbox.id = itemId
+
+itemName.htmlFor = itemId
+itemName.textContent = item
+
+newItem.append(checkbox, itemName, deleteButton)
+list.appendChild(newItem)
+deleteButton.appendChild(deleteIcon)
+
+
+document.getElementById("item").value = ""
+}
